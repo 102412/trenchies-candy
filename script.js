0b86023c0ddb4cd5e4cc9199898359369d1a91b9
102412 <ryland.ritchie@stu.woodford.kyschools.us>
2026-03-16 17:14:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,64 @@
 
 const hero = document.getElementById("heroText");
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll",()=>{
 
 let scroll = window.scrollY;
 
-hero.style.opacity = 1 - scroll / 300;
+hero.style.opacity = 1 - scroll/250;
 
 });
 
 
 
-/* BAG DOWNWARD FLOAT */
+/* BAG FLOAT */
 
 const bag = document.getElementById("bag");
 
-window.addEventListener("scroll", ()=>{
+window.addEventListener("scroll",()=>{
 
 let scroll = window.scrollY;
 
-let move = scroll * 0.1;
+let float = Math.sin(scroll*0.01)*20;
 
-bag.style.transform = `translateY(${move}px)`;
+bag.style.transform=`translateY(${float}px)`;
 
 });
+
+
+
+/* FEATURE REVEAL */
+
+const features=document.querySelectorAll(".feature");
+
+window.addEventListener("scroll",()=>{
+
+let scroll=window.scrollY;
+
+features.forEach((f,i)=>{
+
+if(scroll>400+(i*250)){
+
+f.classList.add("show");
+
+}
+
+});
+
+});
+
+
+
+/* MOBILE MENU */
+
+const btn=document.getElementById("menuBtn");
+const menu=document.getElementById("menu");
+
+btn.onclick=()=>{
+
+menu.style.display=
+menu.style.display==="flex"
+? "none"
+: "flex";
+
+};
